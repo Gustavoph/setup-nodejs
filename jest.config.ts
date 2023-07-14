51de995b74abb26ee2884af844a35c6b785ba50b
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2023-07-14 12:10:55 -0300

    feat: apply eslint in all files

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts',],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   preset: 'ts-jest',
-  testEnvironment: "node",
-  roots: ['src', '__test__'],
-};
+  testEnvironment: 'node',
+  roots: ['src', '__test__']
+}
 
-export default config;
+export default config
--- jest.unit.config.ts
+++ jest.unit.config.ts
@@ -1,4 +1,3 @@
 import config from './jest.config'
 config.testMatch = ['**/*.spec.ts']
 export default config
-
